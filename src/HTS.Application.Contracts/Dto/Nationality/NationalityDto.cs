4a273aa0c7b0acda57af14dc38b4ac90bdd48e58
Murat <baserdem@gmail.com>
2023-03-26 14:30:30 +0300

    Phonecode added

--- src/HTS.Application.Contracts/Dto/Nationality/NationalityDto.cs
+++ src/HTS.Application.Contracts/Dto/Nationality/NationalityDto.cs
@@ -5,6 +5,6 @@ namespace HTS.Dto.Nationality;
 public class NationalityDto: EntityDto<int>
 {
     public string Name { get; set; }
-    public string Code { get; set; }
+    public string PhoneCode { get; set; }
     public bool IsActive { get; set; }
 }

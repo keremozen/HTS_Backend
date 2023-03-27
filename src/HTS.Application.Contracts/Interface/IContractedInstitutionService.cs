6fada5ffb8f79e43695bcfed63688b91f7e59492
obaserdem-jb <ozlemhancioglu@live.com>
2023-03-27 12:10:33 +0300

    Contracted Insititution crud operations implemented

---
+++ src/HTS.Application.Contracts/Interface/IContractedInstitutionService.cs
@@ -0,0 +1,49 @@
+﻿using HTS.Dto;
+using HTS.Dto.Language;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HTS.Dto.Nationality;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+
+namespace HTS.Interface
+{
+    public interface IContractedInstitutionService : IApplicationService
+    {
+        /// <summary>
+        /// Get contracted institution by id
+        /// </summary>
+        /// <param name="id">Desired contracted institution id</param>
+        /// <returns>Desired contracted institution</returns>
+        Task<ContractedInstitutionDto> GetAsync(int id);
+        /// <summary>
+        /// Get all contracted institutions
+        /// </summary>
+        /// <returns>Contracted institution list</returns>
+        Task<PagedResultDto<ContractedInstitutionDto>> GetListAsync();
+        /// <summary>
+        /// Creates contracted institution
+        /// </summary>
+        /// <param name="contractedInstitution">contracted institution information to be insert</param>
+        /// <returns>Inserted nationality</returns>
+        Task<ContractedInstitutionDto> CreateAsync(SaveContractedInstitutionDto contractedInstitution);
+        
+        /// <summary>
+        /// Updates contracted institution
+        /// </summary>
+        /// <param name="id">To be updated contracted institution id</param>
+        /// <param name="contractedInstitution">To be updated information</param>
+        /// <returns>Updated contracted institution object</returns>
+        Task<ContractedInstitutionDto> UpdateAsync(int id, SaveContractedInstitutionDto contractedInstitution);
+        
+        /// <summary>
+        /// Delete given id of contracted institution
+        /// </summary>
+        /// <param name="id">To be deleted contracted institution id</param>
+        /// <returns></returns>
+        Task DeleteAsync(int id);
+    }
+}
---
+++ src/HTS.Application/Service/ContractedInstitutionService.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HTS.Data.Entity;
+using HTS.Dto.Language;
+using HTS.Dto.Nationality;
+using HTS.Interface;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace HTS.Service;
+
+public class ContractedInstitutionService : ApplicationService, IContractedInstitutionService
+{
+    private readonly IRepository<ContractedInstitution, int> _contractedInstitutionRepository;
+    public ContractedInstitutionService(IRepository<ContractedInstitution, int> contractedInstitutionRepository) 
+    {
+        _contractedInstitutionRepository = contractedInstitutionRepository;
+    }
+    
+    public async Task<ContractedInstitutionDto> GetAsync(int id)
+    {
+        return ObjectMapper.Map<ContractedInstitution, ContractedInstitutionDto>(await _contractedInstitutionRepository.GetAsync(id));
+    }
+    
+    public async Task<PagedResultDto<ContractedInstitutionDto>> GetListAsync()
+    {
+        //Get all entities
+        var responseList = ObjectMapper.Map<List<ContractedInstitution>, List<ContractedInstitutionDto>>(await _contractedInstitutionRepository.GetListAsync());
+        var totalCount = await _contractedInstitutionRepository.CountAsync();//item count
+        //TODO:Hopsy Ask Kerem the isActive case 
+        return new PagedResultDto<ContractedInstitutionDto>(totalCount,responseList);
+    }
+
+    public async Task<ContractedInstitutionDto> CreateAsync(SaveContractedInstitutionDto contractedInstitution)
+    {
+        var entity = ObjectMapper.Map<SaveContractedInstitutionDto, ContractedInstitution>(contractedInstitution);
+        await _contractedInstitutionRepository.InsertAsync(entity);
+        return ObjectMapper.Map<ContractedInstitution, ContractedInstitutionDto>(entity);
+    }
+
+    public async Task<ContractedInstitutionDto> UpdateAsync(int id, SaveContractedInstitutionDto contractedInstitution)
+    {
+        var entity = await _contractedInstitutionRepository.GetAsync(id);
+        ObjectMapper.Map(contractedInstitution, entity);
+        return ObjectMapper.Map<ContractedInstitution, ContractedInstitutionDto>( await _contractedInstitutionRepository.UpdateAsync(entity));
+    }
+        
+    public async Task DeleteAsync(int id)
+    {
+        await _contractedInstitutionRepository.DeleteAsync(id);
+    }
+}

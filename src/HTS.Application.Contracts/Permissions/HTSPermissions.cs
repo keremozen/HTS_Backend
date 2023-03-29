a2e0b4b01cc60805271db000514c608c23447bc8
Kerem Özen <keremozen@gmail.com>
2023-03-29 17:04:22 +0300

    Permissions for admin modules are defined

--- src/HTS.Application.Contracts/Permissions/HTSPermissions.cs
+++ src/HTS.Application.Contracts/Permissions/HTSPermissions.cs
@@ -4,6 +4,10 @@ public static class HTSPermissions
 {
     public const string GroupName = "HTS";
 
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public const string HospitalManagement = GroupName + ".HospitalManagement"; 
+    public const string NationalityManagement = GroupName + ".NationalityManagement";
+    public const string LanguageManagement = GroupName + ".LanguageManagement";
+    public const string DocumentTypeManagement = GroupName + ".DocumentTypeManagement";
+    public const string PatientAdmissionMethodManagement = GroupName + ".PatientAdmissionMethodManagement";
+    public const string ContractedInstitutionManagement = GroupName + ".ContractedInstitutionManagement";
 }

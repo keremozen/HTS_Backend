d105e4cefc1c67606aa83797d42600dcc9ee5433
obaserdem-jb <ozlemhancioglu@live.com>
2023-05-28 00:13:44 +0300

    Hospital consultation is updated when hospital responsed. Hospital response is updated when suitable for treatment

--- src/HTS.Application/Service/HospitalResponseService.cs
+++ src/HTS.Application/Service/HospitalResponseService.cs
@@ -6,11 +6,15 @@ using HTS.BusinessException;
 using HTS.Data.Entity;
 using HTS.Dto.HospitalConsultation;
 using HTS.Dto.HospitalResponse;
+using HTS.Dto.HospitalResponseBranch;
+using HTS.Dto.HospitalResponseMaterial;
+using HTS.Dto.HospitalResponseProcess;
 using HTS.Enum;
 using HTS.Interface;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using static HTS.Enum.EntityEnum;
 
 namespace HTS.Service;
 
@@ -19,12 +23,12 @@ public class HospitalResponseService : ApplicationService, IHospitalResponseServ
     private readonly IRepository<HospitalResponse, int> _hospitalResponseRepository;
     private readonly IRepository<HospitalConsultation, int> _hcRepository;
     public HospitalResponseService(IRepository<HospitalResponse, int> hospitalResponseRepository,
-        IRepository<HospitalConsultation, int> hcRepository) 
+        IRepository<HospitalConsultation, int> hcRepository)
     {
         _hospitalResponseRepository = hospitalResponseRepository;
         _hcRepository = hcRepository;
     }
-    
+
     public async Task<HospitalResponseDto> GetAsync(int id)
     {
         var query = (await _hospitalResponseRepository.WithDetailsAsync()).Where(r => r.Id == id);
@@ -40,33 +44,39 @@ public class HospitalResponseService : ApplicationService, IHospitalResponseServ
 
     public async Task CreateAsync(SaveHospitalResponseDto hospitalResponse)
     {
-       await IsDataValidToSave(hospitalResponse);
+        await IsDataValidToSave(hospitalResponse);
         var entity = ObjectMapper.Map<SaveHospitalResponseDto, HospitalResponse>(hospitalResponse);
+        var hConsultation = await _hcRepository.GetAsync(hospitalResponse.HospitalConsultationId);
         if (hospitalResponse.HospitalResponseTypeId == EntityEnum.HospitalResponseTypeEnum.SuitableForTreatment.GetHashCode())
         {
-            //TODO: Özlem ... hospital consultation durumu da suitable for treatment yapılacak.
+            hConsultation.HospitalConsultationStatusId = HospitalConsultationStatusEnum.SuitableForTreatment.GetHashCode();
+            entity.PossibleTreatmentDate = hospitalResponse.PossibleTreatmentDate;
+            entity.HospitalResponseBranches = ObjectMapper.Map<List<SaveHospitalResponseBranchDto>, List<HospitalResponseBranch>>(hospitalResponse.HospitalResponseBranches.ToList());
+            entity.HospitalResponseProcesses = ObjectMapper.Map<List<SaveHospitalResponseProcessDto>, List<HospitalResponseProcess>>(hospitalResponse.HospitalResponseProcesses.ToList());
+            entity.HospitalResponseMaterials = ObjectMapper.Map<List<SaveHospitalResponseMaterialDto>, List<HospitalResponseMaterial>>(hospitalResponse.HospitalResponseMaterials.ToList());
         }
         else
         {
-            hospitalResponse.PossibleTreatmentDate = null;
-            hospitalResponse.HospitalResponseBranches = null;
-            hospitalResponse.HospitalResponseProcesses = null;
-            hospitalResponse.HospitalResponseMaterials = null;
+            entity.PossibleTreatmentDate = null;
+            entity.HospitalResponseBranches = null;
+            entity.HospitalResponseProcesses = null;
+            entity.HospitalResponseMaterials = null;
         }
-        await _hospitalResponseRepository.InsertAsync(entity);
+        hConsultation.HospitalResponses.Add(entity);
+        await _hcRepository.UpdateAsync(hConsultation);
     }
 
     public async Task ApproveAsync(int hospitalResponseId)
     {
         var hr = (await _hospitalResponseRepository.WithDetailsAsync(hr => hr.HospitalConsultation))
             .Where(hr => hr.Id == hospitalResponseId);
-       var entity = await AsyncExecuter.FirstOrDefaultAsync(hr);
-       await IsDataValidToApprove(entity);
-       entity.HospitalConsultation.HospitalConsultationStatusId =
-           EntityEnum.HospitalConsultationStatusEnum.OperationApproved.GetHashCode();
-      await _hospitalResponseRepository.UpdateAsync(entity);
+        var entity = await AsyncExecuter.FirstOrDefaultAsync(hr);
+        await IsDataValidToApprove(entity);
+        entity.HospitalConsultation.HospitalConsultationStatusId =
+            EntityEnum.HospitalConsultationStatusEnum.OperationApproved.GetHashCode();
+        await _hospitalResponseRepository.UpdateAsync(entity);
     }
-    
+
     public async Task RejectAsync(int hospitalResponseId)
     {
         var hr = (await _hospitalResponseRepository.WithDetailsAsync(hr => hr.HospitalConsultation))
@@ -89,7 +99,7 @@ public class HospitalResponseService : ApplicationService, IHospitalResponseServ
             throw new HTSBusinessException(ErrorCode.HospitalResponseTypeNotValidToApprove);
         }
     }
-    
+
     /// <summary>
     /// Checks if entity valid to reject
     /// </summary>
@@ -101,8 +111,8 @@ public class HospitalResponseService : ApplicationService, IHospitalResponseServ
             throw new HTSBusinessException(ErrorCode.HospitalResponseTypeNotValidToReject);
         }
     }
-    
-        
+
+
     /// <summary>
     /// Checks if data is valid to save
     /// </summary>
@@ -115,21 +125,21 @@ public class HospitalResponseService : ApplicationService, IHospitalResponseServ
         {
             throw new HTSBusinessException(ErrorCode.RelationalDataIsMissing);
         }
-        
+
         //There should be only one hospital response
-        if (await  _hospitalResponseRepository.AnyAsync(r => r.HospitalConsultationId == hospitalResponse.HospitalConsultationId))
+        if (await _hospitalResponseRepository.AnyAsync(r => r.HospitalConsultationId == hospitalResponse.HospitalConsultationId))
         {
             throw new HTSBusinessException(ErrorCode.HospitalAlreadyResponsed);
         }
         //TODO:Hopsy response should be correct hospital, authorized uhb user. In the future chech this.
-     
+
         //If status is ok, check data
         if (hospitalResponse.HospitalResponseTypeId == EntityEnum.HospitalResponseTypeEnum.SuitableForTreatment.GetHashCode())
         {
             if (!hospitalResponse.HospitalizationTypeId.HasValue
                  || !(hospitalResponse.HospitalResponseBranches?.Any() ?? false)
-                || ( hospitalResponse.HospitalizationTypeId == EntityEnum.HospitalizationTypeEnum.SurgicalHospitalization.GetHashCode() && !(hospitalResponse.HospitalResponseProcesses?.Any() ?? false))
-                ||  !(hospitalResponse.HospitalResponseMaterials?.Any() ?? false)
+                || (hospitalResponse.HospitalizationTypeId == EntityEnum.HospitalizationTypeEnum.SurgicalHospitalization.GetHashCode() && !(hospitalResponse.HospitalResponseProcesses?.Any() ?? false))
+                || !(hospitalResponse.HospitalResponseMaterials?.Any() ?? false)
                 || hospitalResponse.PossibleTreatmentDate == DateTime.MinValue
                 || hospitalResponse.PossibleTreatmentDate == null
                 || hospitalResponse.HospitalizationNumber == null)

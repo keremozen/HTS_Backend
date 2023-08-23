57c2d2eb7beee985ab23f99e6904049f01b6bbd4
ozlem-hb <ozlemhancioglu@live.com>
2023-08-23 20:44:26 +0300

    DocumentTypeId is made nullable

--- src/HTS.Application.Contracts/Dto/PatientDocument/SavePatientDocumentDto.cs
+++ src/HTS.Application.Contracts/Dto/PatientDocument/SavePatientDocumentDto.cs
@@ -6,7 +6,7 @@ public class SavePatientDocumentDto
 {
     [Required]
     public int PatientId { get; set; }
-    public int DocumentTypeId { get; set; }
+    public int? DocumentTypeId { get; set; }
     public string Description { get; set; }
     [Required, StringLength(100)]
     public string FileName { get; set; }

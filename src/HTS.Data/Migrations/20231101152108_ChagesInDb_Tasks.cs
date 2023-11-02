24793c4c23c6eea336f7b3110752f90cf89d343a
Kerem Özen <keremozen@gmail.com>
2023-11-02 08:50:02 +0300

    Localization changes.. New migration

---
+++ src/HTS.Data/Migrations/20231101152108_ChagesInDb_Tasks.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HTS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChagesInDb_Tasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HTSTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PatientId = table.Column<int>(type: "integer", nullable: false),
+                    TaskTypeId = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    RelatedEntityId = table.Column<int>(type: "integer", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HTSTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_AbpUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_AbpUsers_DeleterId",
+                        column: x => x.DeleterId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_AbpUsers_LastModifierId",
+                        column: x => x.LastModifierId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_AbpUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HTSTasks_TaskTypes_TaskTypeId",
+                        column: x => x.TaskTypeId,
+                        principalTable: "TaskTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_CreatorId",
+                table: "HTSTasks",
+                column: "CreatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_DeleterId",
+                table: "HTSTasks",
+                column: "DeleterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_LastModifierId",
+                table: "HTSTasks",
+                column: "LastModifierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_PatientId",
+                table: "HTSTasks",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_TaskTypeId",
+                table: "HTSTasks",
+                column: "TaskTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HTSTasks_UserId",
+                table: "HTSTasks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HTSTasks");
+
+            migrationBuilder.DropTable(
+                name: "TaskTypes");
+        }
+    }
+}

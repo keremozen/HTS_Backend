ade73c6a55dc6e7ee99bbd47416ae2424d93cb39
Kerem Özen <keremozen@gmail.com>
2023-11-21 09:52:44 +0300

    Added migration. Localization changes

--- src/HTS.Data/Migrations/20231121051939_ChangesInDb_IsENabizColumnAddedToProforma.Designer.cs
+++ src/HTS.Data/Migrations/20231121051939_ChangesInDb_IsENabizColumnAddedToProforma.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HTS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace HTS.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121051939_ChangesInDb_IsENabizColumnAddedToProforma")]
+    partial class ChangesInDb_IsENabizColumnAddedToProforma
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
